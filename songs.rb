c4539c1878800b22de9999dcccebf2f20ed7d3c7
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-06-26 14:27:03 +0100

    test song has name passes

--- songs.rb
+++ songs.rb
@@ -1,5 +1,7 @@
 class Songs 
 
+    attr_reader :track_name
+
     def initialize(track_name)
         @track_name = track_name
     end
--- specs/songs_spec.rb
+++ specs/songs_spec.rb
@@ -6,14 +6,15 @@ require_relative("../songs")
 
 class SongsTest < MiniTest::Test
 
-
     def setup
         @song1 = Songs.new("Wonderwall")
         @song2 = Songs.new("Fernando")
         @song3 = Songs.new("Dancing Queen")
     end
 
-    
+    def test_song_has_name
+        assert_equal("Fernando", @song2.track_name)
+    end
 
 
 end
